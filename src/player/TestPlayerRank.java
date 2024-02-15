378f4e4f197098da2635a500c83c89971bf9b642
simonflenman <simon.flenman1@gmail.com>
2024-02-15 15:48:59 +0100

    Create TestPlayerRank.java
    Unit test för Player Rank

---
+++ src/player/TestPlayerRank.java
@@ -0,0 +1,71 @@
+package player;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestPlayerRank {
+
+    @Test
+    public void testPlayerRankKnightBoundary() {
+        Player player = new Player();
+        player.setNetWorth(1999);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.PEASANT, player.getPlayerRank());
+    }
+
+    @Test
+    public void testPlayerRankKnightBoundaryUpper() {
+        Player player = new Player();
+        player.setNetWorth(2000);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.KNIGHT, player.getPlayerRank());
+    }
+
+    @Test
+    public void testPlayerRankLordBoundary() {
+        Player player = new Player();
+        player.setNetWorth(3999);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.KNIGHT, player.getPlayerRank());
+    }
+
+    @Test
+    public void testPlayerRankLordBoundaryUpper() {
+        Player player = new Player();
+        player.setNetWorth(4000);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.LORD, player.getPlayerRank());
+    }
+
+    @Test
+    public void testPlayerRankKingBoundary() {
+        Player player = new Player();
+        player.setNetWorth(7499);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.LORD, player.getPlayerRank());
+    }
+
+    @Test
+    public void testPlayerRankKingBoundaryUpper() {
+        Player player = new Player();
+        player.setNetWorth(7500);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.KINGS, player.getPlayerRank());
+    }
+
+    @Test
+    public void testPlayerRankNoRankChange() {
+        Player player = new Player();
+        player.setNetWorth(1000);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.PEASANT, player.getPlayerRank());
+    }
+
+    @Test
+    public void testPlayerRankKnightNoChange() {
+        Player player = new Player();
+        player.setNetWorth(3000);
+        player.checkPlayerRank();
+        assertEquals(PlayerRanks.KNIGHT, player.getPlayerRank());
+    }
+}
